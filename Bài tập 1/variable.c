399ec52ff1a3f8e3da10aa001af9daaa23898c3d
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2020-11-12 11:00:49 +0700

    Add files via upload

---
+++ Bài tập 1/variable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void){
+	//khai bao bien num
+	int num ;
+	//khoi tao gia tri num
+	num = 5;
+	//xuat num ra man hinh
+	printf("Bien num co gia tri: %d\n",num);
+	//khai bao bien c
+ 	char c;
+	//khoi tao gia tri c
+	c = 'a';
+	//hien thi c ra man hinh
+	printf("Bien c co gia tri: %c",c);
+	//hien thi bien num = 5 va c = a
+	printf("bien num = %d va bien c = %c",num,c);
+	
+	return 0;
+}
