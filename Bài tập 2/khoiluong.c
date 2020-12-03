931cf4fbbf1bdc1117b08ebeb44d829df0428666
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2020-12-02 23:00:30 -0500

    Create khoiluong.c

---
+++ Bài tập 2/khoiluong.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  int m ;//khai bao bien m
+  printf("nhap m = ");//xuat m
+  scanf("%d", &m);//nhap main
+  printf("kg = %dg\n", m *1000);
+  printf("%d",m);
+  printf("   |%d", m * 1000);
+  return 0;
