76c2a762510e508821bb49afa9b3c27b0405eeab
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2020-12-02 17:50:58 -0800

    Update hinhtron.c

--- Bài tập 1/hinhtron.c
+++ Bài tập 1/hinhtron.c
@@ -2,13 +2,19 @@
 
 int main(void) {
 double DT, CV, R;
+//khai bao bien
 R = 1.5;
+ //gan bien R
 const double PI = 3.14;
+//Khai Bao Hang
 CV = 2 * R * PI;
 DT = R * R * PI;
+// Tinh Chu vi dien tich
 printf("CV = 2 * R * PI = %0.2f\n",CV);
 printf("DT = R * R * PI = %0.3f\n",DT);
+ //hien thi ra man hinh
 printf("R   |  CV  |   DT\n");
 printf("1.5 | 9.42 | 7.065");
+  // tao bang
   return 0;
 }
