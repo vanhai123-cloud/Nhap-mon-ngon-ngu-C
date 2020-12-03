0730f491a50bec6b800a526ca86569acc5f5d72d
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2020-12-02 21:48:47 -0500

    Update hinhtron.c

--- Bài tập 1/hinhtron.c
+++ Bài tập 1/hinhtron.c
@@ -1,13 +1,11 @@
-#include <stdio.h>//khai bao thu vien
-#define PI 3.14159//khai bao hang
-int   main(){
- double DT, CV, R;//khai bao bien
+#include <stdio.h>
+#define PI 3.14;
+int main(void) {
+  double CV, DT,R;
+  R = 1.5;
   CV = 2*R*PI;
   DT = R*R*PI;
-   printf("nhap ban kinh R: ");
-  scanf("%lf", &R);
-  printf("chu vi hinh tron la: %f\n", CV);
-  printf("dien tich hinh tron la: %f", DT);
-  getchar();
-  
+  printf ("chu vi hinh tron la: %f\n", CV);
+  printf ("dien tich hinh tron la: %f", DT);
+  return 0;
 }
