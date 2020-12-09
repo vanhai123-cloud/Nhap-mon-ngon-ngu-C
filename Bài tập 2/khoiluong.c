5e798223002b25cf4d393b8cf6b266adb89463d5
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2020-12-09 21:12:00 +0700

    Update khoiluong.c

--- Bài tập 2/khoiluong.c
+++ Bài tập 2/khoiluong.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+
 int main(){
-  int m ;//khai bao bien m
-  printf("nhap m = ");//xuat m
-  scanf("%d", &m);//nhap main
-  printf("kg = %dg\n", m *1000);
-  printf("%d",m);
-  printf("   |%d", m * 1000);
-  return 0;
+	int m;
+	printf("nhap m = ");
+	scanf("%d", &m);
+	printf("kg = %dg\n",m * 1000);
+	printf("kg = %d|",m * 1);
+  printf("g = %d",m *1000);
+	return 0;
+}
