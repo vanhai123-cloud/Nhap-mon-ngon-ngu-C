96aba26d30b874f16f591c49b10df03a04363257
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2020-12-10 10:15:44 +0700

    Update chanle.c

--- Bài tập 3/chanle.c
+++ Bài tập 3/chanle.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 int main(){
-	int a ;//khai bao bien a
-	printf("nhap so a = ",a);//hien thi a ra man hinh
-	scanf("%d", &a);//nhap a
-	//kiem tra a chan hay le
-	if (a == 0){//dieu kien a
-		printf(" %d bang khong",a);//hien thi a bang khong
-	}else if   (a % 2 == 0){
-		printf("%d la so chan",a);//hien thi a la so chan
+	int n ;//khai bao bien n
+	printf("nhap so n = ",n);//hien thi n ra man hinh
+	scanf("%d", &n);//nhap n
+	//kiem tra n chan hay le
+	if (n == 0){//dieu kien n
+		printf(" %d bang khong",n);//hien thi n bang khong
+	}else if   (n % 2 == 0){
+		printf("%d la so chan",n);//hien thi n la so chan
 	}else {
-		printf("%d la so le",a);//hien thi a la so le
+		printf("%d la so le",n);//hien thi n la so le
 	}
 	return 0;
 }
