679c44c1795beb9237b6e3ee390d6b3ce15aa41f
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2020-12-17 08:14:32 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -8,9 +8,9 @@ int dongia, soluong, thanhtien;
 scanf("%d%d", &dongia, &soluong);
 // dùng lệnh scanf để gán giá trị cho biến
 thanhtien = dongia * soluong;
-if (soluong >= 5){
-  printf("so tien can phai thanh toan la %d", thanhtien *75/100);
-// nếu số lượng mua nhỏ hơn lơn hơn hoặc bằng 5 thì sẽ giảm giá 25%
+if (soluong >= 5 && dongia >= 500){
+  printf("so tien can phai thanh toan la %d", thanhtien * 50/100);
+// nếu số lượng mua nhỏ hơn lơn hơn hoặc bằng 5 và đơn giá lơn hơn hoặc băng 500 thì sẽ giảm giá 50%
 }else{
   printf("so tien can phai thanh toan la %d", thanhtien);
 // Ngược lại số lượng nhỏ hơn 5 thì sẽ không giảm giá
