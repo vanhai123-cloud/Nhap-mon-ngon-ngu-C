0837dbb5072edb2176c75e30b36da9fae21bc091
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2020-12-23 22:10:35 -0500

    Create tong10.c

---
+++ Bài tập 3/tong10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int i;//khai bao bien so
+	int sum = 0;//khai bao bien tong
+	for( i = 0; i < 10; i++)
+		{
+			sum = sum + i;//tinh tong
+		}
+		printf(" sum = %d", sum);//in ra man hinh tong
+	return 0;
+	}
