c5f88aa6889d3370c44c2fd0d4fb93603b1ac45b
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2020-12-24 08:32:20 +0700

    Create XinChao.C

---
+++ Bài tập 3/XinChao.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int count;
+  for(count = 1; count <= 10; count++){
+    //Hien thi ra man hinh 10 lan
+        printf("Xin Chao \n");
+    //dung lenh printf cho may tinh hien thi ra dong xin chao
+  }
+  return 0;
+}
