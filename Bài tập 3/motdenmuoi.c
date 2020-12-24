4677346fa317247e0a57585cb1645165425c7b5e
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2020-12-24 08:58:49 +0700

    Create motdenmuoi.c

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int count;
+  for(count = 1; count <= 10; count++){
+  //cho for bang 1 va bang 10
+       printf("%d ", count);
+  //hien thi ra man hinh tu 1 den 10 theo hang ngang
+  }
+  for(count = 1; count <= 10; count++){
+  //cho for bang 1 va bang 10
+        printf("\n%d", count); 
+  //hien thi ra man hinh tu 1 den 10 theo hang ngang
+  }
+  return 0;
+}
