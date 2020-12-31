4071241b80fbb0e036a0429e56085bc2bbf01dcd
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2020-12-30 20:21:10 -0500

    Create review_01.c

---
+++ Bài tập 3/review_01.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+	int n;/khai bao bien n
+	printf("nhap n: ",n);
+	scanf("%d", &n);
+	if(n<=9){//dieu kien n
+		switch(n){
+			case 1:
+				printf("one");break;
+			case 2:
+				printf("two");break;
+			case 3:
+				printf("three");break;
+			case 4:
+				printf("four");break;
+			case 5:
+				printf("five");break;
+			case 6:
+				printf("six");break;
+			case 7:
+				printf("seven");break;
+			case 8:
+				printf("eight");break;
+			case 9:
+				printf("nine");break;
+		}
+		
+	}else{
+		printf("greater than 9",n);
+	}
+	
+	
+		
+	return 0;
+}
