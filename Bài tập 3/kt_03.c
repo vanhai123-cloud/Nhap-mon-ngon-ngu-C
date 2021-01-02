8ff98d5485013b973a8b57401caea4904ee6145a
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-02 17:57:20 +0700

    Create kt_03.c

---
+++ Bài tập 3/kt_03.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i;
+    for( i = 1; i <= 20; i = i + 2)
+    {
+      if( (i == 5) || (i == 11) || (i == 15))continue;//o day su dung cau lenh continue thay cho break boi vi neu su dung break thi no se ket thuc o dieu kien dau tien con continue se quay lai kiem tra dieu tra dieu kien va thuc hien vong lap tiep theo
+      printf("%d ",i);
+    }
+  
+  return 0;
+}
