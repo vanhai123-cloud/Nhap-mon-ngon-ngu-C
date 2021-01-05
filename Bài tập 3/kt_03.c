5f523284c6b59179b5f30f761d31301fbf227f5b
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2021-01-05 10:11:55 +0700

    Update kt_03.c

--- Bài tập 3/kt_03.c
+++ Bài tập 3/kt_03.c
@@ -2,7 +2,8 @@
 int main (){
 for (int i = 1; i <= 20; i++) {
 //cho giá trị bắt đầu từ 1 và kết thúc khi bằng 20 và tăng dần 1 giá trị
-    if (i != 5 && i != 11 && i != 15){
+    if (i % 2 == 1 && i != 5 && i != 11 && i != 15){
+    // i chia cho 2 dư 1 dể lấy số lẻ 
     //!= là toán tử khác. Nên dòng lệnh này có nghĩa là i khác 5, 11 và 15
        printf("%d ", i);
     //hiển thị i tăng dần 1 giá trị và loại bỏ 5, 11 và 15 
