b4044dcd049c0fb074117b4141e396c19e0be353
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-06 22:17:04 -0500

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -11,3 +11,19 @@ int main(){
   }
   return 0;
 }
+#include<stdio.h>
+
+int main(){
+	printf("--------BANG CUU CHUONG--------\n");
+	int n;
+	n = 2;
+	printf("bang 2\n");
+	printf(" 2*1 = %d\n 2*2 = %d\n 2*3 = %d\n 2*4 = %d\n",n*1,n*2,n*3,n*4);
+	n = 3;
+	printf("bang 3");
+	printf(" \n 3*1 = %d\n 3*2 = %d\n 3*3 = %d\n 3*4 = %d\n",n*1,n*2,n*3,n*4);
+	n = 4;
+	printf("bang 4");
+	printf(" \n 4*1 = %d\n 4*2 = %d\n 4*3 = %d\n 4*4 = %d\n",n*1,n*2,n*3,n*4);
+	return 0;
+}
