ed402975090f7381b8133bd569df938269ca5d6b
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2021-01-07 10:16:31 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main (){
-int a, b, c;
-a = 2, b = 3, c = 4;
-printf("Bang Cuu Chuong 1\n");
-//hiển thị ra dòng lệnh bảng cửu chương 1 
-printf("%d * 1 = %d\n", a, a * 1);
-//cho a * 1 để đc dòng đầu của bảng cửu chương. Các dòng khác tương tự nhưng * 2 nhân 3
-printf("%d * 2 = %d\n", a, a * 2);
-printf("%d * 3 = %d\n", a, a * 3);
-printf("Bang Cuu Chuong 2\n");
-printf("%d * 1 = %d\n", b, b * 1);
-printf("%d * 2 = %d\n", b, b * 2);
-printf("%d * 3 = %d\n", b, b * 3);
-printf("Bang Cuu Chuong 3\n");
-printf("%d * 1 = %d\n", c, c * 1);
-printf("%d * 2 = %d\n", c, c * 2);
-printf("%d * 3 = %d\n", c, c * 3);
+  int a, b, c;
+  a = 2, b = 3, c = 4;
+  printf("Bang Cuu Chuong 1\n");
+  //hiển thị ra dòng lệnh bảng cửu chương 1 
+  printf("%d * 1 = %d\n", a, a * 1);
+  //cho a * 1 để đc dòng đầu của bảng cửu chương. Các dòng khác tương tự nhưng * 2 nhân 3
+  printf("%d * 2 = %d\n", a, a * 2);
+  printf("%d * 3 = %d\n", a, a * 3);
+  printf("Bang Cuu Chuong 2\n");
+  printf("%d * 1 = %d\n", b, b * 1);
+  printf("%d * 2 = %d\n", b, b * 2);
+  printf("%d * 3 = %d\n", b, b * 3);
+  printf("Bang Cuu Chuong 3\n");
+  printf("%d * 1 = %d\n", c, c * 1);
+  printf("%d * 2 = %d\n", c, c * 2);
+  printf("%d * 3 = %d\n", c, c * 3);
     return 0; 
 }
