f61e1333d5b3618ae7cc2c7bb422753c0a2d0665
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2021-01-07 10:14:33 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
-int main () {
-    printf (" === in bang cuu chuong ra man hinh ===\n");
-    //in ra màn hình dòng bảng cưu chương ra màn hình 
-    for (int i = 2; i < 10; i++) {
-        //cho vòng lặp từ 2 đến 9
-        printf ("        -- bang cuu chuong %d --\n", i);
-        //in bảng cửu chương số ... ra màn hình
-        for (int j = 1; j <= 10; j++) {
-        //cho vòng lặp từ 1 đến 10
-        printf ("               %d x %d = %d\n", i, j, i * j);
-        //vì for j nằm trong for i nên khi in ra màn hình thì for i chạy 1 thì for j chạy từ 1 đến 9
-        //khi * thì sẽ ra bảng cửu chương
-    }
-    }
-    return 0;
+int main (){
+int a, b, c;
+a = 1, b = 2, c = 3;
+printf("Bang Cuu Chuong 1\n");
+//hiển thị ra dòng lệnh bảng cửu chương 1 
+printf("%d * 1 = %d\n", a, a * 1);
+//cho a * 1 để đc dòng đầu của bảng cửu chương. Các dòng khác tương tự nhưng * 2 nhân 3
+printf("%d * 2 = %d\n", a, a * 2);
+printf("%d * 3 = %d\n", a, a * 3);
+printf("Bang Cuu Chuong 2\n");
+printf("%d * 1 = %d\n", b, b * 1);
+printf("%d * 2 = %d\n", b, b * 2);
+printf("%d * 3 = %d\n", b, b * 3);
+printf("Bang Cuu Chuong 3\n");
+printf("%d * 1 = %d\n", c, c * 1);
+printf("%d * 2 = %d\n", c, c * 2);
+printf("%d * 3 = %d\n", c, c * 3);
+    return 0; 
 }
