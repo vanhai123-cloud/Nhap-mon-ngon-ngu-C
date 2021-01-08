c7483e52c32010dce4bf242790539aee57da6b1e
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-08 16:38:18 +0700

    Create kt_05.c

---
+++ Bài tập 3/kt_05.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main ()
+{
+int i, n;
+	printf("Nhap so \n");
+
+	scanf("%d",&n);
+        {
+    printf("So le \n");
+        }
+    {
+for(i = 1; i <= n; i = i + 2)
+if(i <= n )
+        {
+    printf("%d ",i);
+        }
+    }
+        {
+    printf("\nSo Chan \n");
+        }
+    {
+    for(i = 2; i <= n;  i = i + 2)
+	if ( i <= n)
+        {
+		printf("%d ",i);
+        }
+    }
+return 0;
+}
