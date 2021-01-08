c32f61c3db1bddb85ababe5a6af0940a4b080425
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-08 17:06:04 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
-
-int main(void) {
-  int i,n,sum;
-  sum = 0;
-  printf("nhap n = ");
-  scanf("%d", &n);
-  for(i=1;i<=n;i++){
-    sum = sum + n;
-  }printf("tong cua %d la %d",n,sum);
-
-  return 0;
+#include<stdio.h>
+int main()
+{
+	int i, n;
+	int S;
+	S = 0;
+	i = 1;
+	printf("\nNhap n: ");
+	scanf("%d", &n);
+	while(i <= n)
+	{
+		S = S + i;
+		i++;
+	}
+	printf("\nTong 1 + 2 + ... + %d la %ld: ", n, S);
+	return 0;
 }
