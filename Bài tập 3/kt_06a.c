18868f734f669991d0bba318ab10b9bdf9ab6800
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2021-01-09 15:35:37 +0700

    Update kt_06a.c

--- Bài tập 3/kt_06a.c
+++ Bài tập 3/kt_06a.c
@@ -13,6 +13,7 @@ int h;
             //hiển thị khoảng trống ra màn hình
         for(int j = 1; j <= 2 * i - 1; j++)
         //vòng lặp bắt đầu từ 1 và kết thúc ở 2 * i -1. (i tăng dần 1 giá trị). Khi 2 * i -1 thì dấu * sẽ tăng dần theo i
+        //không có cái tam giác nào là 1 2 4 6 8 cả nên phải trừ đi 1 thành 1 3 5 7 9.
             printf(" * ");
             // hiển thị dấu * ra màn hình
     printf("\n");
