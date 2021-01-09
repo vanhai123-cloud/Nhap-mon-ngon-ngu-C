cc31504167cfacd0c1c2cdf8f3e27b6ce291aee0
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-08 22:13:23 -0500

    Create kt_06.c

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,14 @@
+// h = 4:4 hang
+  	//hang 1:1 sao 3 khoang trang
+  	//hang 2:3 sao 2 khoang trang
+  	//hang 3:5 sao 1 khoang trang
+  	//hang 4:7 sao 0 khoang trang
+  	
+  	//h = 3
+  	//hang 1:1 sao 2 khoang trang
+  	//hang 2:3 sao 1 khoang trang
+  	//hang 3:5 sao 0 khoang trang
+  	
+  	//h = 2
+  	//hang 1:1 sao 1 khoang trang
+  	//hang 2:3 sao 0 khoang trang
