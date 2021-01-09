dff8326fd57a2d4c74ac70093575c5d6cc3b1b75
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-08 21:37:23 -0500

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -15,3 +15,17 @@ int main()
 	printf("\nTong 1 + 2 + ... + %d la %ld: ", n, S);
 	return 0;
 }
+#include<stdio.h>
+int main(){
+	int n ;
+  printf("nhap n = ");
+  scanf("%d",&n);
+	int s = 0;
+	int so;
+	while(n!=0){
+		so = n % 10;
+		n = n / 10;
+		s = s + so;
+	}printf("%d",s);
+	return 0;
+}
