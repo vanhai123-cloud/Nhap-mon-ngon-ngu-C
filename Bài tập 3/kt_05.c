c58e6ebf24a75cd0c6c32eff2697d376fe01775c
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-13 06:23:48 -0800

    Update kt_05.c

--- Bài tập 3/kt_05.c
+++ Bài tập 3/kt_05.c
@@ -1,30 +1,34 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main ()
-{
-int i, n;
-	printf("Nhap so \n");
+int main() {
+    int n, chan = 0, le = 0;
+    printf("nhap n: ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 == 0)
+            chan = chan + i;
+        if(i % 2 == 1)
+            le = le + i;
+    }
+        printf("tong so chan la: %d \n",chan);
+        printf("tong so le la: %d", le);
+      return 0;
+        
+}#include<stdio.h>
 
-	scanf("%d",&n);
-        {
-    printf("So le \n");
-        }
-    {
-for(i = 1; i <= n; i = i + 2)
-if(i <= n )
-        {
-    printf("%d ",i);
-        }
+int main() {
+    int i = 1, n, chan = 0, le = 0;
+    printf("nhap n: ");
+    scanf("%d", &n);
+    while( i <= n) {
+        if(i % 2 == 0)
+            chan = chan + i;
+        if(i % 2 == 1)
+            le = le + i;
+            i++;
     }
-        {
-    printf("\nSo Chan \n");
-        }
-    {
-    for(i = 2; i <= n;  i = i + 2)
-	if ( i <= n)
-        {
-		printf("%d ",i);
-        }
-    }
-return 0;
+        printf("tong so chan la: %d \n",chan);
+        printf("tong so le la: %d", le);
+      return 0;
+        
 }
