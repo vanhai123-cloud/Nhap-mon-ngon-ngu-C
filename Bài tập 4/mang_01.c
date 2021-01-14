bf7bd9eb6aee9f559d7ce5b1c5281959d21a3d51
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2021-01-14 08:23:28 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main () {
-  int mang[4];
+  int a[4];
   //mảng bắt đầu từ 0 và kết thúc ở bé hơn 4 
-  printf("nhap 4 so n tu ban phim: ");
+  printf("nhap 4 so a tu ban phim: ");
   //hiển thị nhap 4 so tu ban phim ra màn hình
   for (int i = 0; i < 4; i++) {
   //vong lặp bắt đầu từ 0 và bé hơn 4 theo mang[4]
-    scanf("%d", &mang[i]);
+    scanf("%d", &a[i]);
     //nhập số của mảng từ bàn phím
   }
   for (int i = 0; i < 4; i++) {
   //lặp lại vòng lặp i
-    printf("%d ", mang[i]);
+    printf("%d ", a[i]);
     //xuất các giá trị có trong mảng ra màn hình 
   }
   return  0;
