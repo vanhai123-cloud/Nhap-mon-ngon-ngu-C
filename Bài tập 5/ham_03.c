e91e469516608796f5594763fd4c9193fd11e09e
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-27 20:29:32 -0500

    Create ham_03.c

---
+++ Bài tập 5/ham_03.c
@@ -0,0 +1,20 @@
+//****************************************CODE**********
+//Khoi khai bao
+#include<stdio.h>
+
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
+
+// khoi dinh nghia ham
+
+//*************************END CODE*****************************
