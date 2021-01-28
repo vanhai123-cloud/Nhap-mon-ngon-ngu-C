f741373261bbdabf013c32d3c6df2ce0f0f0fd92
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-28 02:09:48 -0500

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -3,11 +3,11 @@
 void Nhap_pt_mang( int a[]);
 int n;
 
-void Hien_thi_pt_mang(int n , int a[]);
+void Hien_thi_pt_mang( int a[]);
 
-void Hien_thi_tong_chan(int n, int a[]);
+void Hien_thi_tong_chan( int a[]);
 
-void Dem_chan_le_0(int n, int a[]);
+void Dem_chan_le_0( int a[]);
  //khoi ham main
 int main(){
   int n;
@@ -15,9 +15,9 @@ int main(){
   scanf("%d", &n);
   int a[n];
   Nhap_pt_mang(a);
- // Hien_thi_pt_mang(a);
-  //Hien_thi_tong_chan(a);
- // Dem_chan_le_0(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
   return 0;
 }
 void Nhap_pt_mang( int a[]){
@@ -26,12 +26,33 @@ void Nhap_pt_mang( int a[]){
     scanf("%d", &a[i]);
   }
 }
-void Hien_thi_pt_mang( int a[], int n){
-
+void Hien_thi_pt_mang( int a[]){
+   printf("Cac phan tu trong mang: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
 }
-void Hien_thi_tong_chan(int n, int a[]){
-
+void Hien_thi_tong_chan( int a[]){
+  int S = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] % 2 == 0) {
+            S += a[i];
+        }
+    }
+    printf("\nTong cac so chan la: %d", S); 
 }
-void Dem_chan_le_0(int n, int a[]){
-
+void Dem_chan_le_0( int a[]){
+  int demchan = 0, demle = 0, dem0 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] % 2 == 0)
+            demchan++;
+        if(a[i] % 2 == 1)
+            demle++;
+        if(a[i] == 0)
+            dem0++;
+    }
+    printf("\nCo tat ca %d so chan, %d so le va %d so 0", demchan, demle, dem0);
 }
