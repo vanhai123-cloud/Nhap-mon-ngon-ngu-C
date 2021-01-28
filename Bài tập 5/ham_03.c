f9ce4f549f0afc91e8212ef5b84d88b9668510ae
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-28 02:08:03 -0500

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,29 +1,41 @@
-#include<stdio.h>
-void Nhap_pt_mang(int n, int a[])
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("a[%d] = ", i);
-        scanf("%d", &a[i]);
-    }
-    
-}
-//nhập các phần tử trong mảng
 
-void Hien_thi_pt_mang(int n, int a[])
-{
-    printf("Cac phan tu trong mang: ");
+//Khoi khai bao
+#include<stdio.h>
+void Nhap_pt_mang( int a[]);
+int n;
+
+void Hien_thi_pt_mang( int a[]);
+
+void Hien_thi_tong_chan( int a[]);
+
+void Dem_chan_le_0( int a[]);
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+ // Hien_thi_pt_mang(a);
+  //Hien_thi_tong_chan(a);
+ // Dem_chan_le_0(a);
+  return 0;
+}
+void Nhap_pt_mang( int a[]){
+  for(int i = 0;i < n; i ++){
+    printf("a[%d] = ", i);
+    scanf("%d", &a[i]);
+  }
+}
+void Hien_thi_pt_mang( int a[]){
+   printf("Cac phan tu trong mang: ");
     for (int i = 0; i < n; i++)
     {
         printf("%d ", a[i]);
     }
-    
 }
-//hiển thị các phần tử có trong mảng
-    
-void Hien_thi_tong_chan(int n, int a[])
-{
-    int S = 0;
+void Hien_thi_tong_chan( int a[]){
+  int S = 0;
     for (int i = 0; i < n; i++)
     {
         if(a[i] % 2 == 0) {
@@ -32,11 +44,8 @@ void Hien_thi_tong_chan(int n, int a[])
     }
     printf("\nTong cac so chan la: %d", S); 
 }
-//tổng chẵn lẽ của các phần tử có trong mảng
-
-void Dem_chan_le_0(int n, int a[])
-{
-    int demchan = 0, demle = 0, dem0 = 0;
+void Dem_chan_le_0( int a[]){
+  int demchan = 0, demle = 0, dem0 = 0;
     for (int i = 0; i < n; i++)
     {
         if(a[i] % 2 == 0)
@@ -47,18 +56,4 @@ void Dem_chan_le_0(int n, int a[])
             dem0++;
     }
     printf("\nCo tat ca %d so chan, %d so le va %d so 0", demchan, demle, dem0);
-    
-}
-//đếm các số chẵn và số lẻ có trong mảng
-
-int main(){
-  int n;
-  printf("Nhap n = ");
-  scanf("%d", &n);
-  int a[n];
-  Nhap_pt_mang(n, a);
-  Hien_thi_pt_mang(n, a);
-  Hien_thi_tong_chan(n, a);
-  Dem_chan_le_0(n, a);
-  return 0;
 }
