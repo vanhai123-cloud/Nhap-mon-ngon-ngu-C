825219d3c0b5e20524a3d1157ab6c5a90a9d4445
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-28 02:05:26 -0500

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,12 +1,13 @@
 //Khoi khai bao
 #include<stdio.h>
-void NhapMang(int a[], int n){
-  for(int i = 0; i < n; i ++){
-    printf("nhap mang a[%d]", i);
-    scanf("%d", &a[i]);
-  }
-}
+void Nhap_pt_mang( int a[]);
+int n;
 
+void Hien_thi_pt_mang(int n , int a[]);
+
+void Hien_thi_tong_chan(int n, int a[]);
+
+void Dem_chan_le_0(int n, int a[]);
  //khoi ham main
 int main(){
   int n;
@@ -14,8 +15,23 @@ int main(){
   scanf("%d", &n);
   int a[n];
   Nhap_pt_mang(a);
-  Hien_thi_pt_mang(a);
-  Hien_thi_tong_chan(a);
-  Dem_chan_le_0(a);
+ // Hien_thi_pt_mang(a);
+  //Hien_thi_tong_chan(a);
+ // Dem_chan_le_0(a);
   return 0;
+}
+void Nhap_pt_mang( int a[]){
+  for(int i = 0;i < n; i ++){
+    printf("a[%d] = ", i);
+    scanf("%d", &a[i]);
+  }
+}
+void Hien_thi_pt_mang( int a[], int n){
+
+}
+void Hien_thi_tong_chan(int n, int a[]){
+
+}
+void Dem_chan_le_0(int n, int a[]){
+
 }
