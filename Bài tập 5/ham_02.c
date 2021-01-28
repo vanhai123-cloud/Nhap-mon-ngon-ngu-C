c776edeaca4a8648c0a5d6e9108e8d2d3abd46b6
vanhai123-cloud <72533557+vanhai123-cloud@users.noreply.github.com>
2021-01-27 22:26:19 -0500

    Update ham_02.c

--- Bài tập 5/ham_02.c
+++ Bài tập 5/ham_02.c
@@ -1,22 +1,24 @@
-//****************************************CODE**********
-//Khoi khai bao
+///Khoi khai bao
 #include<stdio.h>
-int kiemTraAmDuong(int n){
-    return n;}
+int kiem_tra_am_duong(int n);
  //khoi ham main
 int main(){
   int n;
   printf("Nhap n = ");
   scanf("%d", &n);
-  if(kiemTraAmDuong(n) > 0)
+  if(kiem_tra_am_duong(n) > 0)
       printf("n la so nguyen duong ");
-  else if (kiemTraAmDuong(n) < 0)
+  else if (kiem_tra_am_duong(n) < 0)
     printf("n la so nguyen âm");
   else 
     printf("n = 0");
   return 0;
+}//khoi dinh nghia ham
+int kiem_tra_am_duong(int n){
+  if(n>0)
+  return 1;
+  else if(n<0)
+  return -1;
+  else
+  return 0;
 }
-
-// khoi dinh nghia ham
-
-//*************************END CODE*****************************
